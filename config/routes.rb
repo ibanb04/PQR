b428e643fe7c75a6266f2cb2a720c783072e553e
ivan bettin <ibanb040@gmail.com>
2018-05-19 00:27:47 +0000

    se agrego el boton new departamento para el crud de departamentos

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   #Mostrar los datos de la tabla
   get '/departaments', to: 'departaments#index'
   #Crear un nuevo departamento
-  get '/departaments/new', to: 'departaments#new'
+  get '/departaments/new', to: 'departaments#new',as: 'new_departament'
   post '/departaments', to: 'departaments#create'
   #Tomar los valores con el Id
   get '/departaments/:id', to: 'departaments#show', as: 'departament'
+
   get '/departaments/:id/edit', to: 'departaments#edit', as: 'edit_departament'
   #Actualizar valores
   patch 'departaments/:id', to: 'departaments#update'
