93fb422f9078d5a0b5ad9df65aea0f3d1e316e98
Jesus Ramos <fotoramosltda1234@gmail.com>
2018-05-20 16:54:05 -0500

    Se le agregó el boton salir cuando inicia sesion y crud de pqr un 50%

--- app/controllers/pqrs_controller.rb
+++ app/controllers/pqrs_controller.rb
@@ -8,6 +8,35 @@ class PqrsController < ApplicationController
     end
 
     def create
-    
+        @pqr = Pqr.new(pqr_params)
+        if @pqr.save
+            redirect_to pqrs_path
+        else
+            render :new
+        end
     end
+
+    def edit
+        @pqr = Pqr.find(params[:id])
+    end
+
+    def update
+        @pqr = Pqr.find(params[:id])
+        if @pqr.update(pqr_params)
+            redirect_to pqrs_path
+        else
+            render :edit
+        end
+    end
+
+    def delete
+        @pqr = Pqr.find(params[:id])
+        @pqr.destroy
+        redirect_to pqrs_path
+    end
+
+    private
+        def pqr_params
+        params.require(:pqr).permit(:name, :description)
+        end
 end
