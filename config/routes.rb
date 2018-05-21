b1773d033cdd5e76a79fce953d8989867307b47f
Jesus Ramos <fotoramosltda1234@gmail.com>
2018-05-20 19:50:44 -0500

    Crud de la tabla pqr terminado

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   resources :users
   resources :audits
   resources :states
+  resources :pqrs
 
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
