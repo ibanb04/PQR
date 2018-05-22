eefff98db6df36dd7185b41d2f6955f9e39a46eb
Jesus Ramos <fotoramosltda1234@gmail.com>
2018-05-21 22:27:20 -0500

    Crud de la tabla Review terminado

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   resources :audits
   resources :states
   resources :pqrs
-
+  resources :reviews
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
